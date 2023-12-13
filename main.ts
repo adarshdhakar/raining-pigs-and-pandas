8893846d70a7c7d17909d6dbac7c10e05d2f0da5
adarshdhakar <153133538+adarshdhakar@users.noreply.github.com>
2023-12-13 21:58:41 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+player.onChat("run", function () {
+	
+})
+let animal = 0
+while ((0 as any) <= (100 as any)) {
+    mobs.spawn(PIG, pos(0, 10, 0))
+    mobs.spawn(PANDA, pos(0, 10, 0))
+}
+animal += 1
